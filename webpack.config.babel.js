0ab7353d7ddd6cf7767402f2f4bd62a6b5b8acd0
Ilya Pashkov <ilyapashkov@yahoo.com>
2017-08-17 06:49:33 +0400

    v0.0.3

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,11 +1,11 @@
-import path from 'path';
-import webpack from 'webpack';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import SpritesmithPlugin from 'webpack-spritesmith';
-import CleanWebpackPlugin from 'clean-webpack-plugin';
+import path from "path";
+import webpack from "webpack";
+import ExtractTextPlugin from "extract-text-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import SpritesmithPlugin from "webpack-spritesmith";
+import CleanWebpackPlugin from "clean-webpack-plugin";
 
-const isProd = (process.env.NODE_ENV === 'production');
+const isProd = (process.env.NODE_ENV === "production");
 const plugins = [
 	new webpack.HotModuleReplacementPlugin(),
 	new webpack.optimize.CommonsChunkPlugin({
@@ -18,23 +18,23 @@ const plugins = [
 	}),
 	new webpack.NamedModulesPlugin(),
 	new HtmlWebpackPlugin({
-		filename: 'index.html',
-		chunks: ['main', 'commons'],
-		template: path.join(__dirname, '/src/index.pug')
+		filename: "index.html",
+		chunks: ["main", "commons"],
+		template: path.join(__dirname, "/src/index.pug")
 	}),
 	new webpack.ProvidePlugin({
-		$: 'jquery',
-		jQuery: 'jquery',
-		_: 'lodash'
+		$: "jquery",
+		jQuery: "jquery",
+		_: "lodash"
 	}),
 	new SpritesmithPlugin({
 		src: {
-			cwd: path.resolve(__dirname, './src/assets/sprites/'),
-			glob: '*.png'
+			cwd: path.resolve(__dirname, "./src/assets/sprites/"),
+			glob: "*.png"
 		},
 		target: {
-			image: path.resolve(__dirname, './src/assets/sprite.png'),
-			css: path.resolve(__dirname, './src/styles/tools/sprite.scss')
+			image: path.resolve(__dirname, "./src/assets/sprite.png"),
+			css: path.resolve(__dirname, "./src/styles/tools/sprite.scss")
 		},
 		apiOptions: {
 			cssImageRef: "~sprite.png"
@@ -42,13 +42,13 @@ const plugins = [
 	})
 ];
 
-isProd ? plugins.push(new ExtractTextPlugin("css/styles.[hash].css"), new CleanWebpackPlugin(['./build'])) : null;
+isProd ? plugins.push(new ExtractTextPlugin("css/styles.[hash].css"), new CleanWebpackPlugin(["./build"])) : null;
 
 export default {
-	entry: path.join(__dirname, './src/app.js'),
+	entry: path.join(__dirname, "./src/app.js"),
 	output: {
-		path: path.join(__dirname, './build/'),
-		filename: 'js/bundle.[hash].js'
+		path: path.join(__dirname, "./build/"),
+		filename: "js/bundle.[hash].js"
 	},
 	plugins,
 	devServer: {
@@ -58,91 +58,74 @@ export default {
 		overlay: true,
 		hot: true
 	},
-	devtool: 'source-map',
+	devtool: "source-map",
 	module: {
 		rules: [
 			//---------------------JS----------------------//
 			{
 				test: /\.(js|jsx)?$/,
-				use: ['babel-loader'],
-				include: path.join(__dirname, '/src'),
+				use: ["babel-loader"],
+				include: path.join(__dirname, "/src"),
 			},
 			//---------------------VUE----------------------//
 			{
 				test: /\.vue?$/,
-				use: 'vue-loader',
+				use: "vue-loader",
 			},
 			//--------------------PUG--------------------//
 			{
 				test: /\.pug$/,
-				loader: 'pug-loader',
+				loader: "pug-loader",
 				options: {pretty: true}
 			},
-			//---------------------CSS---------------------//
+			//---------------------Styles---------------------//
 			{
-				test: /\.css$/,
+				test: /\.(sass|scss|css)$/,
 				use: isProd ?
 					ExtractTextPlugin.extract({
 						fallback: "style-loader",
 						use: [
-							{loader: 'css-loader', options: {sourceMap: true}},
-							{loader: 'postcss-loader', options: {sourceMap: true}}
+							{loader: "css-loader", options: {sourceMap: true}},
+							{loader: "postcss-loader", options: {sourceMap: true}},
+							{loader: "sass-loader", options: {sourceMap: true}}
 						]
 					})
 					: [
-						{loader: 'style-loader', options: {sourceMap: true}},
-						{loader: 'css-loader', options: {sourceMap: true}},
-						{loader: 'postcss-loader', options: {sourceMap: true}}
-					]
-			},
-			//---------------------SASS---------------------//
-			{
-				test: /\.(sass|scss)$/,
-				use: isProd ?
-					ExtractTextPlugin.extract({
-						fallback: "style-loader",
-						use: [
-							{loader: 'css-loader', options: {sourceMap: true}},
-							{loader: 'postcss-loader', options: {sourceMap: true}},
-							{loader: 'sass-loader', options: {sourceMap: true}}
-						]
-					})
-					: [
-						{loader: 'style-loader', options: {sourceMap: true}},
-						{loader: 'css-loader', options: {sourceMap: true}},
-						{loader: 'postcss-loader', options: {sourceMap: true}},
-						{loader: 'sass-loader', options: {sourceMap: true}}
+						{loader: "style-loader", options: {sourceMap: true}},
+						{loader: "css-loader", options: {sourceMap: true}},
+						{loader: "postcss-loader", options: {sourceMap: true}},
+						{loader: "sass-loader", options: {sourceMap: true}}
 					]
 			},
 			//--------------------Fonts--------------------//
 			{
 				test: /\.(woff2|ttf|eot)$/,
-				use: 'file-loader?name=assets/fonts/[name].[ext]&publicPath=../'
+				use: "file-loader?name=assets/fonts/[name].[ext]&publicPath=../"
 			},
 			//--------------------Images--------------------//
 			{
 				test: /\.(png|jpg|jpeg|gif)$/,
-				use: 'file-loader?name=assets/images/[name].[ext]&publicPath=../'
+				use: "file-loader?name=assets/images/[name].[ext]&publicPath=../"
 			},
 			//--------------------Video--------------------//
 			{
 				test: /\.mp4$/,
-				use: 'file-loader?name=assets/video/[name].[ext]&publicPath=../'
+				use: "file-loader?name=assets/video/[name].[ext]&publicPath=../"
 			},
 			//---------------------SVG---------------------//
 			{
 				test: /\.svg$/,
-				include: path.join(__dirname, './src/assets/icons'),
+				include: path.join(__dirname, "./src/assets/icons"),
 				use: [
-					'svg-sprite-loader?' + JSON.stringify({
-						name: '[name]',
+					"svg-sprite-loader?" + JSON.stringify({
+						name: "[name]",
 						prefixize: true
 					}),
-					'svgo-loader?' + JSON.stringify({
+					"svgo-loader?" + JSON.stringify({
 						plugins: [
 							{
 								addClassesToSVGElement: {
-									className: 'icon'
+									className: "icon"
 								}
 							},
 							{removeTitle: true},
@@ -155,6 +138,6 @@ export default {
 		]
 	},
 	resolve: {
-		modules: ['node_modules', 'assets']
+		modules: ["node_modules", "assets"]
 	}
-}
+};
